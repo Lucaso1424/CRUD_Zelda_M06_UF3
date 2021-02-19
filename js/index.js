c40af10e614ee32f71a0298debb6794c1be770ff
lucaso1424 <lucaspadilla648@gmail.com>
2021-02-19 15:46:06 +0100

    lph - merge pablo

--- js/index.js
+++ js/index.js
@@ -1,34 +1,28 @@
-let zelda = {
-    "nom":"Zelda",
-    "id":0,
-} 
-
-window.onload = function() {
-    recargaLista("guardados");
-}
-
-function CreacionTarjeta() {
-    let tarjeta = document.createElement("div");
-    let img = document.createElement("img");
-    let div = document.createElement("div");
-    let span = document.createElement("span");  
-    
-    img.setAttribute("src", "img/trifuerza.jpg", "width", "300", "height", "200");
-    div.setAttribute("class", "flex-column center");
-    span.innerText = "Trifuerza";
-    
-    tarjeta.appendChild(img);
-    tarjeta.appendChild(div);
-    tarjeta.appendChild(span);
-
-    tarjeta.setAttribute("class","flex-row space-between item-activity");
-    
-    return tarjeta;
-}
-
-
-function recargaLista(menu){
-    document.getElementById(menu).innerHTML ="";
-    document.getElementById(menu).appendChild(CreacionTarjeta());
-
-}
+window.onload = function(){
+    // recargaLista("guardados");
+ }
+ 
+ function CreacionTarjeta(){
+     let tarjeta = document.createElement("div");
+     let img = document.createElement("img");
+     let div = document.createElement("div");
+     let span = document.createElement("span");
+     
+     img.setAttribute("src", "img/trifuerza.jpg");
+     div.setAttribute("class", "flex-column center");
+     span.innerText = "Trifuerza";
+     
+     tarjeta.appendChild(img);
+     tarjeta.appendChild(div);
+     tarjeta.appendChild(span);
+ 
+     tarjeta.setAttribute("class","flex-row space-between item-activity");
+     
+     return tarjeta;
+ }
+ 
+ 
+ function recargaLista (menu) {
+     document.getElementById(menu).innerHTML ="";
+     document.getElementById(menu).appendChild(CreacionTarjeta());
+ }
