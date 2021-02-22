b3b2ced42dd8055827ebde9bff8a9fb146f3cc49
Pceballosza <pceballosza@gmail.com>
2021-02-22 13:39:20 +0100

    pcz - commit Mostrar img

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ window.onload = function () {
   var btnPulsado = document.getElementById("guardarForm");
   btnPulsado.addEventListener("click", saveObject);
 
-  generarTabla();
+  //generarTabla();
   document.getElementById("formulario").style.visibility = "visible";
 
 
@@ -46,8 +46,28 @@ function generarTabla() {
 }
 
 /* AÑADIR VALORES FORM AL JSON  */
+function generarItems(){
+  let body = document.getElementById("guardados");
+  let tabla = document.createElement("table");
+  let tbody = document.createElement("tbody");
 
+  for(let i = 0; i < objetoForm.length;i++){
+    let tr = document.createElement("tr");
 
+    for(let j = 0; j < objetoForm[i].length;j++){
+      let objeto = objetoForm[i];
+      let id = objeto.id;
+      let name = objeto.name;
+      let namePlayer = objeto.namePlayer;
+      let radialMenu = objeto.radialMenu;
+      let vehiculo = objeto.vehiculo;
+      console.log(id,name,namePlayer,radialMenu,vehiculo);
+
+      let celda = document.createElement('td');
+      let textCelda = document.createTextNode(name);
+  }
+}
+}
 function saveObject() {
 
   let checkedBoxes = document.querySelectorAll('.txtVehiculo1:checked');
@@ -65,3 +85,42 @@ function saveObject() {
 
     addItems(id, nombreJuego, nombreJugador, menuRadial, vehiculo);
 }
+
+
+function pasarImgBase64(img){
+  let canvas = document.createElement('canvas');
+  let ctx = canvas.getContext('2d');
+
+  canvas.width = img.width;
+  canvar.heigh = img.heigh;
+
+  ctx.drawImage(img,0,0);
+  return canvas.toDataURL('image/jpg');
+}
+
+
+let img = document.querySelector('#guardarForm');
+img.addEventListener('load', function(param){
+  let dataURL = getDataUrl(param.currentTarget);
+  console.log(param.currentTarget);
+  console.log(dataURL);
+
+});
+
+
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
+}
+
